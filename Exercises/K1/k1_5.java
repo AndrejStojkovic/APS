26deb015d594f8dde413dedcf99aa34de2f21c59
Andrej Stojkovikj <andrejstojkovikj@yahoo.com>
2023-11-11 13:59:14 +0100

    Added alternative version to K1 5

--- Exercises/K1/k1_5.java
+++ Exercises/K1/k1_5.java
@@ -3,13 +3,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Stack;
 
+// Bez STACK
 public class ExpressionEvaluator {
-
     public static int evaluateExpression(String expression){
-        // Vasiot kod tuka
         int res = 0;
 
         String [] add = expression.split("\\+");
@@ -31,5 +29,35 @@ public class ExpressionEvaluator {
         BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
         System.out.println(evaluateExpression(input.readLine()));
     }
+}
 
+// So STACK
+public class ExpressionEvaluator {
+    public static int evaluateExpression(String expression){
+        int res = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        String [] add = expression.split("\\+");
+
+        for (String s : add) {
+            String[] mult = s.split("\\*");
+            int m = 1;
+
+            for (String string : mult) {
+                m *= Integer.parseInt(string);
+            }
+
+            stack.add(m);
+        }
+
+        while(!stack.isEmpty()) {
+            res += stack.pop();
+        }
+
+        return res;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(evaluateExpression(input.readLine()));
+    }
 }
