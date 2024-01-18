1ea08c768d031f4508a6a8204b8bf74b34c57c30
Andrej Stojkovikj <andrejstojkovikj@yahoo.com>
2024-01-18 23:58:10 +0100

    Added K2 2 and 3

---
+++ Exercises/K2/k2_2.java
@@ -0,0 +1,158 @@
+// K2 2
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class MapEntry<K extends Comparable<K>,E> implements Comparable<K> {
+
+    // Each MapEntry object is a pair consisting of a key (a Comparable
+    // object) and a value (an arbitrary object).
+    K key;
+    E value;
+
+    public MapEntry (K key, E val) {
+        this.key = key;
+        this.value = val;
+    }
+
+    public int compareTo (K that) {
+        // Compare this map entry to that map entry.
+        @SuppressWarnings("unchecked")
+        MapEntry<K,E> other = (MapEntry<K,E>) that;
+        return this.key.compareTo(other.key);
+    }
+
+    public String toString () {
+        return "<" + key + "," + value + ">";
+    }
+}
+
+class SLLNode<E> {
+    protected E element;
+    protected SLLNode<E> succ;
+
+    public SLLNode(E elem, SLLNode<E> succ) {
+        this.element = elem;
+        this.succ = succ;
+    }
+
+    @Override
+    public String toString() {
+        return element.toString();
+    }
+}
+
+class CBHT<K extends Comparable<K>, E> {
+
+    // An object of class CBHT is a closed-bucket hash table, containing
+    // entries of class MapEntry.
+    private SLLNode<MapEntry<K,E>>[] buckets;
+
+    @SuppressWarnings("unchecked")
+    public CBHT(int m) {
+        // Construct an empty CBHT with m buckets.
+        buckets = (SLLNode<MapEntry<K,E>>[]) new SLLNode[m];
+    }
+
+    private int hash(K key) {
+        return Math.abs(key.hashCode()) % buckets.length;
+    }
+
+    public SLLNode<MapEntry<K,E>> search(K targetKey) {
+        // Find which if any node of this CBHT contains an entry whose key is
+        // equal
+        // to targetKey. Return a link to that node (or null if there is none).
+        int b = hash(targetKey);
+        for (SLLNode<MapEntry<K,E>> curr = buckets[b]; curr != null; curr = curr.succ) {
+            if (targetKey.equals(((MapEntry<K, E>) curr.element).key))
+                return curr;
+        }
+        return null;
+    }
+
+    public void insert(K key, E val) {		// Insert the entry <key, val> into this CBHT.
+        MapEntry<K, E> newEntry = new MapEntry<K, E>(key, val);
+        int b = hash(key);
+        for (SLLNode<MapEntry<K,E>> curr = buckets[b]; curr != null; curr = curr.succ) {
+            if (key.equals(((MapEntry<K, E>) curr.element).key)) {
+                // Make newEntry replace the existing entry ...
+                curr.element = newEntry;
+                return;
+            }
+        }
+        // Insert newEntry at the front of the 1WLL in bucket b ...
+        buckets[b] = new SLLNode<MapEntry<K,E>>(newEntry, buckets[b]);
+    }
+
+    public void delete(K key) {
+        // Delete the entry (if any) whose key is equal to key from this CBHT.
+        int b = hash(key);
+        for (SLLNode<MapEntry<K,E>> pred = null, curr = buckets[b]; curr != null; pred = curr, curr = curr.succ) {
+            if (key.equals(((MapEntry<K,E>) curr.element).key)) {
+                if (pred == null)
+                    buckets[b] = curr.succ;
+                else
+                    pred.succ = curr.succ;
+                return;
+            }
+        }
+    }
+
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < buckets.length; i++) {
+            temp += i + ":";
+            for (SLLNode<MapEntry<K,E>> curr = buckets[i]; curr != null; curr = curr.succ) {
+                temp += curr.element.toString() + " ";
+            }
+            temp += "\n";
+        }
+        return temp;
+    }
+
+}
+
+public class KumanovskiDijalekt {
+    public static void main (String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+                System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        CBHT<String, String> hash = new CBHT<>(Math.max(1, N));
+        String rechnik[]=new String[N];
+
+        for(int i=0;i<N;i++){
+            rechnik[i]=br.readLine();
+            String [] r = rechnik[i].split(" ");
+            hash.insert(r[0], r[1]);
+        }
+
+        String tekst=br.readLine();
+        String [] text = tekst.split(" ");
+
+        for (String s : text) {
+            char end = s.charAt(s.length() - 1);
+            boolean startsWithUppercase = Character.isUpperCase(s.charAt(0));
+
+            String targetWord = s;
+            if(!Character.isLetter(end)) {
+                targetWord = s.substring(0, s.length() - 1);
+            }
+
+            SLLNode<MapEntry<String, String>> node = hash.search(targetWord.toLowerCase());
+
+            if(node == null) {
+                System.out.print(s + " ");
+                continue;
+            }
+
+            targetWord = node.element.value;
+            if(startsWithUppercase) targetWord = targetWord.substring(0, 1).toUpperCase() + targetWord.substring(1);
+            if(!Character.isLetter(end)) targetWord += end;
+
+            System.out.print(targetWord + " ");
+        }
+    }
+}
